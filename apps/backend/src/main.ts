88139cb9be618a704b2475d07f602f4dd253661f
Edward <e.arechwa@gmail.com>
2025-01-01 14:11:29 +0100

    feat: add docker for db

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env);
+  console.log(process.env.PORT);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 8000);
 }
